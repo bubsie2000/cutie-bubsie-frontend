5d412ee0ac824889bc872408ab94cc42ef6665d0
bubsie2000 <winniefoss@outlook.com>
2023-11-15 19:50:18 +0800

    Connected sign up form to backend

---
+++ app/components/signupform.tsx
@@ -0,0 +1,72 @@
+'use client';
+
+import React from 'react';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function SignUpForm() {
+    const router = useRouter()
+
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('')
+
+    const handleSignUp = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        const user = {
+            firstName, lastName, email, password
+        }
+
+        // Sign Up
+        const res = await fetch('http://localhost:3000/user', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(user)
+        })
+
+        if (res.status === 201) {
+            console.log(res.body)
+            router.push('/login')
+        }
+    }
+
+  return (
+    <form onSubmit={handleSignUp}>
+          <input
+              type="text"
+              required
+              placeholder="First Name"
+              className="input w-full max-w-xs"
+              value={firstName}
+              onChange={(e)=> setFirstName(e.target.value)}
+          />
+          <input
+              type="text"
+              required
+              placeholder="Last Name"
+              className="input w-full max-w-xs"
+              value={lastName}
+              onChange={(e)=> setLastName(e.target.value)}
+          />
+          <input
+              type="email"
+              required
+              placeholder="Email"
+              className="input w-full max-w-xs"
+              value={email}
+              onChange={(e)=> setEmail(e.target.value)}
+          />
+          <input
+              type="password"
+              required
+              placeholder="Password"
+              className="input w-full max-w-xs"
+              value={password}
+              onChange={(e)=> setPassword(e.target.value)}
+          />
+    <input type="submit" value="SIGN UP" className="btn text-white btn-primary btn-wide mb-5"/>
+  </form>
+  )
+}
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import Character02 from 'public/Character_02.png';
 import Link from 'next/link';
 import type { Metadata } from 'next';
+import SignUpForm from '../components/signupform';
 
 export const metadata: Metadata = {
   title: 'Sign Up',
@@ -15,13 +16,7 @@ export default function SignUpPage() {
       <Image className='object-cover object-top rounded-3xl w-1/2 mx-auto mt-8 mb-4 md:my-0 md:w-full md:h-full block bg-yellow-200 md:rounded-none' src={Character02} alt='Drawing of a girl holding a Nintendo Switch. The girl has light pink skin and purple hair up in two buns. She is wearing a Japanese sailor style uniform. ' />
       <div className="prose md:my-auto text-center max-w-none mb-8">
         <h1 className="text-pink-200 text-5xl md:text-7xl text-center">SIGN UP</h1>
-        <form action="/" id="signUpForm">
-          <input type="text" required placeholder="First Name" className="input w-full max-w-xs" />
-          <input type="text" required placeholder="Last Name" className="input w-full max-w-xs" />
-          <input type="email" required placeholder="Email" className="input w-full max-w-xs" />
-          <input type="password" required placeholder="Password" className="input w-full max-w-xs" />
-          <input type="submit" value="SIGN UP" className="btn text-white btn-primary btn-wide mb-5"/>
-        </form>
+        <SignUpForm />
         <Link href="/login" className="link text-yellow-200 hover:text-yellow-400 text-lg">Already have an account? Log In</Link>
       </div>
     </div>
